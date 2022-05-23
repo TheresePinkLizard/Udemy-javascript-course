5680783c5e5c9467d98561eb06a821158fbc5766
TheresePinkLizard <101266648+TheresePinkLizard@users.noreply.github.com>
2022-05-23 13:59:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -71,4 +71,81 @@ console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best
 */
 
 //------OBJECT METHODS
+/*
+'strict';
+
+const jonas = {
+    firstName: 'jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher', // string value
+    friends: ['michael', 'peter', 'steven'], //array value. array er en annen type objekt
+    hasDriversLicence: true, //boolean value
+// function inni objekt, bruker :,  utenfor objekt blir det const calcAge = (birthyear)
+// function value
+    // calcAge: function(birthYear) { 
+    //     return 2037 - birthYear;
+    // }
+    //annen versjon, bedre, automatisk henter alder
+    // calcAge: function() { 
+    //     return 2037 - this.birthYear; //this  refererer til jonas, siden den er inni objektet, 
+    //     //prøv å unngå repetisjon, derfor gjør det slik
+    // }
+    calcAge: function() {  // lagre alder slik at den ikke trenger å kalkulerer funksjon om og om igjen
+        this.age = 2037 - this.birthYear; //  lignende funksjon som property jonas.location
+        return this.age; 
+       
+    }, // husk komma her
+    //challenge. legger til : for å kalkulere om jonas har førerkort eller ikke
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicence ? 'a' : 'no'} drivers licence`
+    }
+    // ved å bruke this kan man endre navn på objekt uten å måtte endre flere steder
+};
+//dot
+console.log(jonas.calcAge()); // svar er 46, alderen i 2037
+//bracket, trenger å være string ''
+// console.log(jonas['calcAge'](1991)); //prøv å unngå repetisjon, derfor gjør det slik
+console.log(jonas.calcAge());
+// når kalkuleringen er gjort og man trenger å vise resultatet flere ganger så kan man i stedet 
+// kan lagre svaret slik at den ikke må gjøre funksjonen om og om igjen
+
+console.log(jonas.age); // mye bedre, effektiv
+
+//------challenge
+/*
+// "jonas is a 46 year old teacher, and he has a drivers licence" (a or no)
+// skisse. den fungerte, men kursholder ønsket noe annet
+if (jonas.hasDriversLicence) {
+    console.log(`${jonas.firstName} is a ${jonas.age} year old ${jonas.job}, and has a drivers licence `)
+} else {
+    console.log(`${jonas.firstName} is a ${jonas.age} year old ${jonas.job}, and does not have a drivers licence `)
+}
+
+console.log(jonas.getSummary());
+*/
+
+//-----CODING CHALLENGE 3
+
+const mark = {
+    name:'mark',
+    mass: 78,
+    height: 1.69,
+
+    calcBMI = function() {
+        this.markBMI = [mark.mass] / [mark.height] ** 2;
+        return 
+    }
+}
+const john = {
+    name:'john',
+    mass: '92',
+    height: '1.95',
+
+
+    calcBMI = function() {
+        
+    }
+}
+
 
