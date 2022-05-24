afcc2f129e117bf9a049c7446e8c163e88c33a21
TheresePinkLizard <101266648+TheresePinkLizard@users.noreply.github.com>
2022-05-24 11:52:53 +0200

    hkhjhh

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ const mark = {
     mass: 78,
     height: 1.69,
 
-    calcBMI = function() {
+    calcBMI: function() {
         this.bmi = this.mass / this.height ** 2;
         return this.bmi
     }
